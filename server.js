72d43b85d28e6f3f4cbcf795e0cd7067b4e0eb15
Praveen Kumar Pandit <praveenpandit25@navgurukul.org>
2026-05-15 16:37:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json({ limit: "1mb" }));
 app.use(cors());
